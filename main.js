a39ebdd5444fe3a7d281519060117540de939fca
volupert <volupert@gmail.com>
2026-02-20 13:36:38 +0000

    fix: update disqus shortname to tes2-1 and add noscript tag

--- main.js
+++ main.js
@@ -213,7 +213,7 @@ var disqus_config = function () {
 */
 (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://test-9i5.disqus.com/embed.js';
+    s.src = 'https://tes2-1.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
 })();
